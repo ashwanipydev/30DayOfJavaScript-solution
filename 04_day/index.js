9440a7192c9b45451e856fad6c917170ab64e540
ashwani <pydev.ashwani@gmail.com>
2023-10-09 10:51:38 +0530

    done

--- 04_day/index.js
+++ 04_day/index.js
@@ -45,20 +45,86 @@ let b = 3;
 //         console.log(`Enter Number is ${evenOrOdd}. It's odd number`);
 // }
 
-const scores = 85;
+// let num = prompt('Enter number');
+// switch (true) {
+//   case num > 0:
+//     console.log('Number is positive');
+//     break;
+//   case num == 0:
+//     console.log('Numbers is zero');
+//     break;
+//   case num < 0:
+//     console.log('Number is negative');
+//     break;
+//   default:
+//     console.log('Entered value was not a number');
+// }
 
-if(isNaN(scores)){
-        console.log('Plz enter a valid scores');
-}else{
-        let grad
-        switch (scores) {
-                case scores >= 80 && scores <= 100:
-                        grad = "A";
-                        console.log(grad);
-                        break;
-        
-                default:
-                        break;
-        }
+
+// let weather = 'cloudy'
+// switch (weather) {
+//   case 'rainy':
+//     console.log('You need a rain coat.')
+//     break
+//   case 'cloudy':
+//     console.log('It might be cold, you need a jacket.')
+//     break
+//   case 'sunny':
+//     console.log('Go out freely.')
+//     break
+//   default:
+//     console.log(' No need for rain coat.')
+// }
+
+// Switch More Examples
+// let dayUserInput = prompt('What day is today ?')
+// let day = dayUserInput.toLowerCase()
+
+// switch (day) {
+//   case 'monday':
+//     console.log('Today is Monday')
+//     break
+//   case 'tuesday':
+//     console.log('Today is Tuesday')
+//     break
+//   case 'wednesday':
+//     console.log('Today is Wednesday')
+//     break
+//   case 'thursday':
+//     console.log('Today is Thursday')
+//     break
+//   case 'friday':
+//     console.log('Today is Friday')
+//     break
+//   case 'saturday':
+//     console.log('Today is Saturday')
+//     break
+//   case 'sunday':
+//     console.log('Today is Sunday')
+//     break
+//   default:
+//     console.log('It is not a week day.')
+// }
+
+
+
+
+const score = parseFloat(prompt('Enter the student\'s score:'));
+
+switch (true){
+        case score >= 80 && score <= 100:
+                console.log('A')
+                break
+        case score >= 70 && score <= 79:
+                console.log('B')
+                break
+        case score >= 60 && score <= 69:
+                console.log('C')
+                break
+        case score >= 50 && score <= 59:
+                console.log('D')
+                break
+        default:
+                console.log("F");
+                break
 }
-
